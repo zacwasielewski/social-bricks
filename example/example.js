4bf40472756ccb6b0d4634e553094eec3097b020
Zac Wasielewski <zac@wasielewski.org>
2013-08-05 12:07:19 -0400

    Improve example code

--- example/example.js
+++ example/example.js
@@ -1,18 +1,18 @@
-var container = $('.social-bricks');
+var container = $('#social-bricks-container');
 
 container.socialbricks({
 	networks: [
 		//{name:'linkedin',id:'buddy-media'},
-		{ name:'facebook', id:'sunyit', access_token:'550907728298677|TKbJGxmXgDgRMbeEVSxvZcWQWtE' },
+		//{ name:'facebook', id:'', access_token:'' },
 		//{name:'pinterest',id:'potterybarn'},
 		//{name:'twitter',id:'in1dotcom'},
 		//{name:'googleplus',id:'105588557807820541973/posts'},
 		{ name:'rss', url:'http://feeds.feedburner.com/good/lbvp' },
-		{ name:'rss', url:'http://www.makebetterwebsites.com/feed/' },
 		//{name:'craigslist',id:'boo',areaName:'southcoast'},
-		{ name:'rss', url:'https://www.facebook.com/feeds/page.php?format=rss20&id=7092621023', secure:'detect' }
+		{ name:'rss', url:'https://github.com/zacwasielewski.atom', secure:'detect' },
+		{ name:'rss', url:'http://www.sunyit.edu/apps/calendar/export.php?type=rss&sponsortype=all&timebegin=today&categoryid=8' }
 	],
-	max_items: 20,
+	max_items: 20
 });
 
 //container.socialbricks("fetch");
